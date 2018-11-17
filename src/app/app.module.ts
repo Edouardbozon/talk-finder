e5ed54fa9e6bcd007cac802a0d5eb9045bcb6557
edouardbozon <bozonedouard@gmail.com>
2018-11-17 12:32:00 +0100

    Add pwa support and material lib

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,15 @@
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [],
+  imports: [ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }), BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
